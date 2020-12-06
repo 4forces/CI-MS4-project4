1ad63867452a8b264483ec8d627678162328d78b
4forces <toh.tingji@gmail.com>
2020-12-06 22:37:36 +0000

    Add 'products/index.template.html' to products/views.py

--- products/views.py
+++ products/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Products App")
+    # return HttpResponse("Welcome to Products App")
+    return render(request, 'products/index.template.html')
