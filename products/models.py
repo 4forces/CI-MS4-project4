6536b74fdae380a2586f9a2eb1c4883a996b901e
4forces <toh.tingji@gmail.com>
2020-12-08 05:56:48 +0000

    Comment out products model while working on other apps

--- products/models.py
+++ products/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(blank=False, max_length=150)
-    cost = models.FloatField(blank=False)
-    supplier = models.ForeignKey('Supplier',
-                                 blank=True,
-                                 null=True,
-                                 on_delete=models.CASCADE)
-    unit = models.CharField(blank=False, max_length=10)
-    quantity = models.IntegerField(blank=False, default=0)
-    category = models.ForeignKey('Category',
-                                 blank=True,
-                                 null=True,
-                                 on_delete=models.CASCADE)
-    part_number = models.CharField(blank=False,
-                                   max_length=15)
-    desc = models.TextField(blank=False)
+# class Product(models.Model):
+#     name = models.CharField(blank=False, max_length=150)
+#     cost = models.FloatField(blank=False)
+#     supplier = models.ForeignKey('Supplier',
+#                                  blank=True,
+#                                  null=True,
+#                                  on_delete=models.CASCADE)
+#     unit = models.CharField(blank=False, max_length=10)
+#     quantity = models.IntegerField(blank=False, default=0)
+#     category = models.ForeignKey('Category',
+#                                  blank=True,
+#                                  null=True,
+#                                  on_delete=models.CASCADE)
+#     part_number = models.CharField(blank=False,
+#                                    max_length=15)
+#     desc = models.TextField(blank=False)
