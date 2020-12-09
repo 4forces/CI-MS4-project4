326fee6be2efd81c0bdd1c12072d6e123e30328c
4forces <toh.tingji@gmail.com>
2020-12-09 13:27:04 +0000

    Add 'Supplier' and 'Category' models

--- products/models.py
+++ products/models.py
@@ -1,19 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=150)
     cost = models.FloatField(blank=False)
-    # supplier = models.ForeignKey('Supplier', blank=True, null=True,
-    #                              on_delete=models.CASCADE)
     unit = models.CharField(blank=False, max_length=10)
     quantity = models.IntegerField(blank=False, default=0)
-    # category = models.ForeignKey('Category', blank=True, null=True,
-    #                              on_delete=models.CASCADE)
+    category = models.ForeignKey('Category',
+                                 on_delete=models.CASCADE, default=1)
     part_number = models.CharField(blank=False, max_length=15)
     desc = models.TextField(blank=False)
+    buyer = models.ForeignKey(User,
+                              on_delete=models.CASCADE, default=1)
+    supplier = models.ForeignKey('Supplier',
+                                 on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
 
+
+class Supplier(models.Model):
+    supplier_name = models.CharField(blank=False, max_length=30)
+    country = models.CharField(blank=True, max_length=90)
+
+    def __str__(self):
+        return self.supplier_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(blank=False, max_length=30)
+
+    def __str__(self):
+        return self.category_name
