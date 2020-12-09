743e5f60d11955402ec905e9b1cb976c49ebbd6e
4forces <toh.tingji@gmail.com>
2020-12-09 13:28:35 +0000

    Point to correct file/paths in 'product' views.py and urls.py

--- products/views.py
+++ products/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Welcome to Products App")
-    return render(request, 'products/index.template.html')
+    return render(request, 'products/products.template.html')
