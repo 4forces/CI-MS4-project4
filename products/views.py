caf399dcc42295f62186bf4063aef02168f350a5
4forces <toh.tingji@gmail.com>
2020-12-12 13:11:28 +0000

    Done 'view_single_item' page and related fxn (views.py)

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ def view_shop(request):
     })
 
 
-def view_single_product(request):
+def view_single_product(request, product_id):
     # return HttpResponse("Welcome to Products App")
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/product.template.html', {
