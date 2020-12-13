54d2d12380f4871fad06e2565fdeb8278927a9c4
4forces <toh.tingji@gmail.com>
2020-12-13 03:16:18 +0000

    Done Search Bar in products-shop page non-functional display only

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Product, Category
 
+
 class SearchForm(forms.Form):
-    name = forms.CharField(max_length=150, required=False)
+    name = forms.CharField(max_length=150, required=False,
+                           label="Search product by name")
     category = forms.ModelChoiceField(queryset=Category.objects.all(),
-                                      required=False)
+                                      required=False, label="Select Category")
