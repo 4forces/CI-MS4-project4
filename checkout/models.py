6d735156432c438aa744b70c7da8085ec172697c
4forces <toh.tingji@gmail.com>
2020-12-24 14:59:46 +0000

    Done checkout/models.py and migrate

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from products.models import Product
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Purchase(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField(blank=False)
+    qty = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "Purchase for product "
+        f"#{self.product.id} by user#{self.user.id}"
